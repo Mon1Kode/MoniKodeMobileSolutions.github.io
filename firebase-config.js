3d8987768336cf7288ceda766504922f8f18006a
Victor Delamonica <victor.delamonica@epitech.eu>
2024-04-26 11:06:17 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig ={
+  apiKey: 'AIzaSyDgGQJALa9LfQv77658MqQF4DOIudMsqgo',
+  appId: '1:672036530039:android:3753883399ea6bc51ca372',
+  messagingSenderId: '672036530039',
+  projectId: 'la-passerelle-220fa',
+  databaseURL: 'https://la-passerelle-220fa-default-rtdb.europe-west1.firebasedatabase.app',
+  storageBucket: 'la-passerelle-220fa.appspot.com',
+};
+
+export default firebaseConfig;
