511c674a8023a5e265dfa457d30b39792d96856b
Victor Delamonica <victor.delamonica@epitech.eu>
2024-04-26 11:24:41 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
-const firebaseConfig ={
-  apiKey: 'AIzaSyDgGQJALa9LfQv77658MqQF4DOIudMsqgo',
-  appId: '1:672036530039:android:3753883399ea6bc51ca372',
-  messagingSenderId: '672036530039',
-  projectId: 'la-passerelle-220fa',
-  databaseURL: 'https://la-passerelle-220fa-default-rtdb.europe-west1.firebasedatabase.app',
-  storageBucket: 'la-passerelle-220fa.appspot.com',
+const firebaseConfig = {
+  apiKey: "AIzaSyBjAUM6a4Sn2XQb1vBJFJYQ1NfrxHGjczM",
+  authDomain: "la-passerelle-220fa.firebaseapp.com",
+  databaseURL: "https://la-passerelle-220fa-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "la-passerelle-220fa",
+  storageBucket: "la-passerelle-220fa.appspot.com",
+  messagingSenderId: "672036530039",
+  appId: "1:672036530039:web:251ac6bf5445ed651ca372",
+  measurementId: "G-ZJ8TFPKZ2B"
 };
 
 export default firebaseConfig;
